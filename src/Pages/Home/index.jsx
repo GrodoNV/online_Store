98de4e7b0811377a9c34da7372c0fd3700f4d78e
GrodoNV <grodrigonvargas@gmail.com>
2024-07-20 17:19:49 -0500

    Conectando la api de fake store API , y agregando las variables de title o category o image o price

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,10 +1,26 @@
+import { useState, useEffect } from 'react';
 import Layout from '../../Components/Layout/index'
 import Card from "../../Components/Card"
 function Home() {
+  const [items, setItems] = useState(null);
+  
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products')
+      .then(response => response.json())
+      .then(data => setItems(data))
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);
+
   return (
     <Layout>
       Home
-      <Card />
+      <div className='grid gap-4 grid-cols-4 w-full max-w-screen-lg'>
+      {
+        items?.map((item)=>(<Card key={item.id} data={item}/>
+        ))
+      }
+      </div>
+
     </Layout>
   );
 }
