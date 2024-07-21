75770d3bdc4fe317d6a21a92b6e6d14ddd630c0c
GrodoNV <grodrigonvargas@gmail.com>
2024-07-21 11:04:21 -0500

    agregando el carrito de compras (icono),cambiando de div a button el simbolo de + tambien haciendo un contador de elementos cada que el usuario haga click sobre el +

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -1,6 +1,11 @@
 import { NavLink } from "react-router-dom"
+import { useContext } from "react"
+import { ShoppingCartContext } from "../../Context"
 const Navbar = ()=>{
+    const context = useContext(ShoppingCartContext)
     const activeStyle ='underline underline-offset-4'
+    
+
     return(
         <nav className="flex justify-between items-center fixed z-10 top-0 w-full py-5 px-8 text-sm font-light">
             <ul className="flex items-center gap-3">
@@ -97,7 +102,7 @@ const Navbar = ()=>{
                     </NavLink>
                 </li>
                 <li>
-                    0
+                    🛒 {context.count}
                 </li>
             </ul>
         </nav>
