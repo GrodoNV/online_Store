8d7aaa9f6d9979db609d5cc4a54878aff10e2fa3
GrodoNV <grodrigonvargas@gmail.com>
2024-07-23 08:18:29 -0500

    creando pagina my-orders para alojar la lista de varias compras hechas en diferentes fechas o demas , tambien redireccionando con link , al ingresar a hacer un pedido o checkout , y darle al boton de atras (<) este nos llevara a /my-orders mientras que donde se encuentra la lista de compras es en my-order

--- src/Pages/MyOrder/index.jsx
+++ src/Pages/MyOrder/index.jsx
@@ -2,11 +2,19 @@ import Layout from '../../Components/Layout/index'
 import { useContext } from 'react';
 import { ShoppingCartContext } from '../../Context';
 import OrderCard from '../../Components/OrderCard';
+import { Link } from 'react-router-dom';
+import { ChevronLeftIcon } from '@heroicons/react/24/solid';
 function MyOrder() {
     const context = useContext(ShoppingCartContext)
     return (
         <Layout>
-            MyOrder
+            <div className='flex items-center justify-center relative w-80 mb-6' >
+                <Link to='/my-orders' className='absolute left-0'>
+                    <ChevronLeftIcon className='h-6 w-6 text-black cursor-pointer'/>
+                </Link>
+                <h1>My Order</h1>
+            </div>
+
             <div className='flex flex-col w-80'>
             {
                 context.order && context.order.length > 0 ?
--- src/Pages/MyOrders/index.jsx
+++ src/Pages/MyOrders/index.jsx
@@ -1,8 +1,29 @@
 import Layout from '../../Components/Layout/index'
+import { Link } from 'react-router-dom';
+import OrdersCard from '../../Components/OrdersCard';
+import { useContext } from 'react';
+import { ShoppingCartContext } from '../../Context';
 function MyOrders() {
+    const context = useContext(ShoppingCartContext)
+
     return (
         <Layout>
-            MyOrders
+            <div className='flex items-center justify-center relative w-80 ' >
+                
+                <h1>MyOrders</h1>
+            </div>
+            {
+                context.order.map((order, index)=>{
+                    <Link key={index} to={`/my-orders/${order.id}`}>
+                        <OrdersCard 
+                        totalPrice={order.totalPrice} 
+                        totalProducts = {order.totalProducts}
+                        />                        
+                    </Link>
+                    
+                })
+            }
+
         </Layout>
     );
 }
